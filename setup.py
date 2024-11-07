21475c17d1ca16f089521360e112683d68deafa5
Mitchell Shibilski-Unkel <133730558+MitchellShibilski-Unkel@users.noreply.github.com>
2024-11-07 10:38:22 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     author_email="",
     description="Optimized Gramformer",
     long_description="A framework for detecting, highlighting, and correcting grammatical errors on natural language text. Built off the original Gramformer by Prithiviraj Damodaran.",
-    url="https://github.com/PrithivirajDamodaran/Gramformer.git",
+    url="https://github.com/MitchellShibilski-Unkel/Optimized-Gramformer.git",
     packages=setuptools.find_packages(),
     #install_requires=['transformers', 'sentencepiece==0.1.95', 'python-Levenshtein==0.12.2', 'fuzzywuzzy==0.18.0',  'tokenizers==0.10.2', 'fsspec==2021.5.0', 'lm-scorer==0.4.2', 'errant'],
     install_requires=['transformers', 'sentencepiece', 'python-Levenshtein', 'fuzzywuzzy',  'tokenizers', 'fsspec', 'errant', 'torch],
