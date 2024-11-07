18019e7a5fef9f8a9c4634f54e0abd5f39d4fb5c
Mitchell Shibilski-Unkel <133730558+MitchellShibilski-Unkel@users.noreply.github.com>
2024-11-07 10:37:50 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import setuptools
 
 setuptools.setup(
-    name="gramformer",
+    name="opt-gramformer",
     version="1.0",
-    author="prithiviraj damodaran",
+    author="Mitchell Shibilski-Unkel",
     author_email="",
-    description="Gramformer",
-    long_description="A framework for detecting, highlighting and correcting grammatical errors on natural language text",
+    description="Optimized Gramformer",
+    long_description="A framework for detecting, highlighting, and correcting grammatical errors on natural language text. Built off the original Gramformer by Prithiviraj Damodaran.",
     url="https://github.com/PrithivirajDamodaran/Gramformer.git",
     packages=setuptools.find_packages(),
     #install_requires=['transformers', 'sentencepiece==0.1.95', 'python-Levenshtein==0.12.2', 'fuzzywuzzy==0.18.0',  'tokenizers==0.10.2', 'fsspec==2021.5.0', 'lm-scorer==0.4.2', 'errant'],
     install_requires=['transformers', 'sentencepiece', 'python-Levenshtein', 'fuzzywuzzy',  'tokenizers', 'fsspec', 'errant', 'torch],
     classifiers=[
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.10",
         "License :: MIT",
         "Operating System :: OS Independent",
     ],
